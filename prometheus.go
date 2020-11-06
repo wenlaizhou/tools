fe3bec790cc048aff6da2f5e748948dd5a80acce
wenlai <wenlai_zhou@126.com>
2020-11-06 18:56:55 +0800

    新增prometheus查询

--- prometheus.go
+++ prometheus.go
@@ -1,1 +1,49 @@
 package tools
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/wenlaizhou/middleware"
+	"time"
+)
+
+type PromResult struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     []struct {
+			Metric map[string]string `json:"metric"`
+			Value  []string          `json:"value"`
+		} `json:"result"`
+	} `json:"data"`
+}
+
+func PromQuery(service string, express string) (PromResult, error) {
+	result := PromResult{}
+	code, _, body, err := middleware.Get(fmt.Sprintf(`%s/api/v1/query?query=%s`, service, express))
+	if err != nil {
+		return result, err
+	}
+	if code != 200 {
+		return result, errors.New(fmt.Sprintf("code is %v", code))
+	}
+	err = json.Unmarshal(body, &result)
+	return result, err
+}
+
+func PromQueryRange(service string, express string, step string, begin time.Time, end time.Time) (PromResult, error) {
+	result := PromResult{}
+	beginStr := begin.Format("2006-01-02T15:04:05.000Z") //time.RFC3339)
+	endStr := end.Format("2006-01-02T15:04:05.000Z")
+	queryUrl := fmt.Sprintf("%s/api/v1/query_range?query=%s&start=%s&end=%s&step=%s", service, beginStr, endStr, step)
+	code, _, body, err := middleware.Get(queryUrl)
+	if err != nil {
+		return result, err
+	}
+	if code != 200 {
+		return result, errors.New(fmt.Sprintf("code is %v", code))
+	}
+	err = json.Unmarshal(body, &result)
+	return result, err
+}
